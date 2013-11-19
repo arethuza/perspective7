31819b6b4cc4ef985cb8fa418b06b4a4964b3412
Alan Slater <alan@high-beyond.com>
2013-11-19 22:32:28 +0000

    More work on actionable and unit tests

--- actionable.py
+++ actionable.py
@@ -2,17 +2,42 @@ import inspect
 
 class Actionable():
 
-    actions = []
+    def invoke(self, verb, user_auth, **kwargs):
+        match_found = False
+        for _, f, action_verb, action_auth, action_args in self.__class__.actions:
+            if action_verb == verb:
+                if len(action_args) == 0:
+                    match_found = True
+                else:
+                    for name, value in action_args.items():
+                        if name in kwargs and kwargs[name] == value:
+                            # We know the value of parameter 'name', so don't bother passing it into f
+                            del kwargs[name]
+                            match_found = True
+                            break
+            if match_found:
+                return f(self, **kwargs)
 
 
-def action(func):
-    func.is_action = True
-    return func
+
+class Action:
+    def __init__(self, verb, authorization, **kwargs):
+        self.verb = verb
+        self.authorization = authorization
+        self.args = kwargs
+    def __call__(self, f):
+        def wrapped(*args, **kwargs):
+            return f(*args, **kwargs)
+        _, line_number = inspect.getsourcelines(f)
+        wrapped.action_spec = line_number, wrapped, self.verb, self.authorization, self.args
+        return wrapped
 
 
-def class_with_actions(cls):
-   for name, method in inspect.getmembers(cls):
-        if hasattr(method, "is_action") and method.is_action:
-            cls.actions.append(method)
-   return cls
-
+def WithActions(cls):
+    cls.actions = []
+    members = inspect.getmembers(cls)
+    for name, method in members:
+        if hasattr(method, "action_spec"):
+            cls.actions.append(getattr(method, "action_spec"))
+    cls.actions.sort(key=lambda action_spec: action_spec[0])
+    return cls
--- tests/actionable_tests.py
+++ tests/actionable_tests.py
@@ -1,27 +1,35 @@
 import unittest
-from actionable import action, class_with_actions, Actionable
+from actionable import WithActions, Action, Actionable
 
-@class_with_actions
+@WithActions
 class ActionableTest(Actionable):
 
-    @action
-    def action0(self):
-        pass
-
-    @action
+    @Action("put", "write")
     def action1(self):
-        pass
+        return 1
 
-    @action
+    @Action("post", "edit")
     def action2(self):
-        pass
+        return 2
+
+    @Action("get", "read", foo='bar')
+    def action3(self):
+        return 3
+
+    @Action("get", "read")
+    def action0(self):
+        return 0
 
 class ActionableTests(unittest.TestCase):
 
     def test_single_action(self):
         at = ActionableTest()
         at.action0()
-        self.assertEqual(len(ActionableTest.actions), 3)
+        self.assertEqual(len(ActionableTest.actions), 4)
+        self.assertEqual(at.invoke("get", "read"), 0)
+        self.assertEqual(at.invoke("put", "write"), 1)
+        self.assertEqual(at.invoke("post", "edit"), 2)
+        self.assertEqual(at.invoke("get", "read", foo="bar"), 3)
 
 
 if __name__ == '__main__':
