a63d33c961ad5db302d2e9adbe75c1f819cbd6d7
Alan Slater <alan@high-beyond.com>
2014-06-09 19:05:11 +0100

    All unit tests working again!

--- unit_tests/tests_file_manager.py
+++ unit_tests/tests_file_manager.py
@@ -41,7 +41,7 @@ class FileManagerTests(unittest.TestCase):
         file_version, file_length, file_hash = file_manager.write_file_data(handle.item_id, None, b'0000000000', user_handle)
         self.assertEquals(0, file_version)
         self.assertEquals(10, file_length)
-        self.assertEquals("84d9c4b849506b6d8f8075a9000e7e0a254be71060ea889fad3c88395988f4fc", file_hash)
+        self.assertEquals("30e26d585a255e74b26f46809f0b67d149443f3e372d1536afc21864431270e4", file_hash)
 
     def test_write_file_data_large(self):
         file_manager = FileManager()
@@ -52,7 +52,7 @@ class FileManagerTests(unittest.TestCase):
         file_version, file_length, file_hash = file_manager.write_file_data(handle.item_id, None, data, user_handle)
         self.assertEquals(0, file_version)
         self.assertEquals(13000000, file_length)
-        self.assertEquals("6ecd7e962d52419a6c649393b54b41ad95c2f700161b3f2470d7d16cc0aa101e", file_hash)
+        self.assertEquals("5c03fc7510021b52b0aacf899290a32e5c19218bf693a509fa4039a813101644", file_hash)
 
 if __name__ == '__main__':
     unittest.main()
